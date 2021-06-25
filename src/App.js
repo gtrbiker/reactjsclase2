40f4bdd3dd31dda3c008392a1703525a43a491a6
will.mz <wilfredomzambrano@gmail.com>
2021-06-25 01:26:01 -0300

    second commit

--- src/App.js
+++ src/App.js
@@ -17,17 +17,17 @@ class App extends React.Component{
         <ShoppingCart />
         
         <CardComponent name={'Cheescake'} 
-                      description={'Precio: '}
+                      description={'Precio: 400 '}
                       
                       img={'https://cdn.kiwilimon.com/recetaimagen/36007/th5-640x426-43954.jpg'} />
 
         <CardComponent name={'Tiramisu'} 
-                      description={'Precio: '}
+                      description={'Precio: 300'}
                       
                       img={'https://www.cocinavital.mx/wp-content/uploads/2019/05/tiramisu-clasico-pasos.jpg'} />
 
         <CardComponent name={'Cupcakes'} 
-                      description={'Precio: '}
+                      description={'Precio: 200'}
                       
                       img={'https://cdn.kiwilimon.com/recetaimagen/10996/th5-640x426-23726.jpg'} />     
         
